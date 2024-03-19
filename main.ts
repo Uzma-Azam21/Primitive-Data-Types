7696b1dc21414edd674cb3c767e87b097f4c931b
Uzma-Azam21 <uzmaazam241@gmail.com>
2024-03-19 00:06:53 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,45 @@
+//    Student Name: Uzma Azam
+//    Student registration no :00330608
+//    Day : Tuesday (9:00 am)
+
+// Class Assignment-1; 
+// Variables
+// A variable is a container thart stores a value.
+// keywords of variable ( var, let , const ) 
+// 1. Var is a globally scoped while let and const are block scope.
+// 2. Var can be updated and re-declared within its scope
+// 3. Let can be updated butnot re-declared.
+// 4. Const can neither be updated nor be re-declared.
+``
+// Examples of primitive data types
+    
+// 1. String
+var  myString : string = "I love programming";
+console.log(myString);
+
+// 2. Number
+var myNumber: number = 2024;
+console.log(myNumber);
+
+// 3. Boolean 
+var a = true;
+var b = false;
+console.log(a,b);
+
+// 4. Null 
+var n = null;
+console.log(n);
+
+// 5. Undefined
+var und = undefined;
+console.log(und);
+
+// 6. Symbol
+let  symbol = "unique"
+console.log (symbol);
+
+// 7. BigInt
+let bigIntvalue = (45729008);
+console.log (bigIntvalue);
+ 
+
